60537d92dd3e689cf290f321578f54491c0573fe
Wei Xia <xw900812@gmail.com>
2016-05-15 00:51:50 -0400

    update

--- NodeSchool/javascripting/scope.js
+++ NodeSchool/javascripting/scope.js
@@ -15,7 +15,7 @@
 //    var a = 4;    // a is a global variable, it can be accessed by the functions below
 //
 //    function foo() {
-//        var b = a * 3;    // b cannot be accessed outside foo function, but can be accessed by functions  
+//        var b = a * 3;    // b cannot be accessed outside foo function, but can be accessed by functions
 //                        // defined inside foo
 //        function bar(c) {
 //        var b = 2;  // another `b` variable is created inside bar function scope
@@ -44,6 +44,7 @@ var a = 1, b = 2, c = 3;
 
          (function secondFunction(){
              var b = 8;
+             console.log("a: "+a+", b: "+b+", c: "+c);
 
              (function thirdFunction(){
                  var a = 7, c = 9;
@@ -55,7 +56,3 @@ var a = 1, b = 2, c = 3;
              })();
          })();
      })();
-
-firstFunction();
-
-console.log("a: "+a+", b: "+b+", c: "+c);
